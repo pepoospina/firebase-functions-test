6e6a0a6ec4fe8158e013873671e97fd855bab4bf
Pepo <pepo.ospina@gmail.com>
2024-04-26 10:42:44 +0200

    added task. Tryting to debug (hanged)

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -8,22 +8,38 @@
  */
 
 import { onRequest } from "firebase-functions/v2/https";
+import { onTaskDispatched } from "firebase-functions/v2/tasks";
 import * as logger from "firebase-functions/logger";
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = onRequest(async (request, response) => {
+exports['helloWorld'] = onRequest(async (request, response) => {
   logger.info("Hello logs!", { structuredData: true });
   const functionResponse = await fetch(
     "http://127.0.0.1:5001/sensenets-9ef26/us-central1/byeWorld"
   );
+
+  await fetch(
+    "http://127.0.0.1:5001/sensenets-9ef26/us-central1/aTask", {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+      },  
+      body: JSON.stringify({data: true}),
+    }
+  );
+
   response.send(
     "Hello from Firebase!" + JSON.stringify(await functionResponse.text())
   );
 });
 
-export const byeWorld = onRequest((request, response) => {
+exports['byeWorld'] = onRequest((request, response) => {
   logger.info("Bye logs!", { structuredData: true });
   response.send("Bye from Firebase!");
+});
+
+exports['aTask'] = onTaskDispatched({}, (request) => {
+  logger.info("a Task!", { structuredData: true });
 });
